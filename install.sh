892ce5a150eab86df9749abcbb059687cdf8efaf
osennyaya <supersduby@gmail.com>
2021-06-26 01:57:35 +0800

    add spacebar

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ ln -s ${PWD}/aria2_rpc ~/.config
 # Rime
 # ln -s ./Rime ~/Library
 
-# skhd & yabai
+# skhd & yabai & spacebar
 ln -s ${PWD}/skhd ~/.config
 ln -s ${PWD}/yabai ~/.config
-
+ln -s ${PWD}/spacebar ~/.config
 
 cd ./aria2_rpc && ./mac_install.sh
 
