54e43d65170a11b22d11438f2268fcfa8458d7a6
osennyaya <supersduby@gmail.com>
2021-09-19 11:07:22 +0800

    add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font_with_fallback({
+    "Monaco for Powerline",
+    "Sarasa Mono SC Nerd",
+  }),
+  font_size = 18,
+  line_height=1.2,
+  harfbuzz_features = {"liga=0"},
+
+  use_ime = true,
+
+  window_decorations = "RESIZE",
+  window_padding = {
+    left = 10,
+    right = 5,
+    top = 10,
+    bottom = 0,
+  },
+  --skip_close_confirmation_for_processes_named = {
+  --  "bash", "sh", "zsh", "fish", "tmux"
+  --}, 
+  window_close_confirmation = "NeverPrompt",
+  enable_tab_bar = false,
+
+  mouse_bindings = {
+    {
+      event={Drag={streak=1, button="Left"}},
+      mods="ALT",
+      action="StartWindowDrag"
+    },
+  }
+}
