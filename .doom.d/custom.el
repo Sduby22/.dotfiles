4fd9fbbf278e1a7bece7f111f3cf2a2c36f5276c
osennyaya <supersduby@gmail.com>
2021-11-20 16:50:38 +0800

    doom emacs

---
+++ .doom.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mac-auto-ascii-mode t)
+ '(mac-auto-operator-composition-max-length 4)
+ '(mac-auto-operator-composition-mode t)
+ '(mac-right-option-modifier 'none)
+ '(menu-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
