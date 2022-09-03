ce515deacff46d7f8b8b8f0d1cc4f95c167a15e8
osennyaya <supersduby@gmail.com>
2022-09-03 21:36:32 +0800

    update lspsaga

--- nvim/lua/modules/completion/config.lua
+++ nvim/lua/modules/completion/config.lua
@@ -17,6 +17,11 @@ end
 function config.nvim_cmp()
   local cmp = require('cmp')
   local snippy = require("snippy")
+  local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+  end
 
   cmp.setup({
     snippet = {
@@ -31,7 +36,14 @@ function config.nvim_cmp()
       ['<C-j>'] = cmp.mapping.select_next_item(),
       ['<C-k>'] = cmp.mapping.select_prev_item(),
       ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm({ select = false })
+      ['<CR>'] = cmp.mapping.confirm({ select = false }),
+      ["<Tab>"] = vim.schedule_wrap(function(fallback)
+        if cmp.visible() and has_words_before() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          fallback()
+        end
+      end),
     },
     sources = {
       {name = 'snippy'}, {name = 'nvim_lsp'}, {name = 'cmdline'},
