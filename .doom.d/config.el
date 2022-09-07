835699a81cbf52201ccf3849eb924d7c4d104a56
osennyaya <supersduby@gmail.com>
2022-09-07 23:58:10 +0800

    evil fcitx

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -25,8 +25,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-(setq doom-font (font-spec :family "Fira Code" :size 16))
+(setq doom-theme 'doom-one-light)
+(setq doom-font (font-spec :family "Fira Code" :size 14))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -59,8 +59,6 @@
 (define-key evil-visual-state-map (kbd "j") 'evil-next-visual-line)
 (define-key evil-visual-state-map (kbd "k") 'evil-previous-visual-line)
 (setq lsp-pyright-python-executable-cmd "python3")
-(global-wakatime-mode)
-(global-tree-sitter-mode)
 ;; ;; to debug with DAP-MODE
 ;; (setq dap-auto-configure-mode t)
 ;; (require 'dap-cpptools)
@@ -73,13 +71,15 @@
   (setq org-hugo-base-dir "~/Documents/blog/")
 )
 
-(require 'dap-cpptools)
-
 (setq url-proxy-services
    '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
      ("http" . "127.0.0.1:7890")
-     ("Https" . "127.0.0.1:7890")))
+     ("https" . "127.0.0.1:7890")))
 
+(global-wakatime-mode)
+(global-tree-sitter-mode)
+;; (fcitx-aggressive-setup)
+;; ;; (setq fcitx-remote-command "fcitx5-remote")
 (use-package! copilot
   ;; :hook (prog-mode . copilot-mode)
   :bind (("C-TAB" . 'copilot-accept-completion-by-word)
@@ -89,9 +89,6 @@
          ("TAB" . 'copilot-accept-completion)))
 ;; (setq copilot-node-executable "/home/sduby/.nvm/versions/node/v17.9.1/bin/node")
 
-;; (setq fcitx-remote-command "fcitx5-remote")
-(fcitx-aggressive-setup)
-
 (add-hook 'org-mode-hook #'org-modern-mode)
 (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
 (setq org-hide-emphasis-markers t)
@@ -99,3 +96,4 @@
 (setq lsp-rust-analyzer-cargo-watch-command "clippy")
 
 (setq company-auto-select-first-candidate nil)
+(require 'evil-fcitx)
