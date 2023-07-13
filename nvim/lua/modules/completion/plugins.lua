0ba209908efd4c88cd4a5c881e83ebb0dd0e38e2
sduby <supersduby@gmail.com>
2023-07-13 21:59:27 +0800

    update

--- nvim/lua/modules/completion/plugins.lua
+++ nvim/lua/modules/completion/plugins.lua
@@ -51,7 +51,61 @@ plugin({
 plugin({ 'echasnovski/mini.pairs', version = '*', opts = {}, event = 'InsertCharPre' })
 
 plugin({
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    local opts = {
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = '[[',
+          jump_next = ']]',
+          accept = '<CR>',
+          refresh = 'gr',
+          open = '<M-CR>',
+        },
+        layout = {
+          position = 'bottom', -- | top | left | right
+          ratio = 0.4,
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = false,
+          accept_word = false,
+          accept_line = false,
+          next = false,
+          prev = false,
+          dismiss = false,
+        },
+      },
+      filetypes = {
+        markdown = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ['.'] = false,
+      },
+      copilot_node_command = 'node', -- Node.js version must be > 16.x
+      server_opts_overrides = {},
+    }
+
+    require('copilot').setup(opts)
+    vim.keymap.set('i', '<Tab>', function()
+      if require('copilot.suggestion').is_visible() then
+        require('copilot.suggestion').accept()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
+      end
+    end, { desc = 'Super Tab' })
+  end,
 })
 
 -- plugin({
--- nvim/lua/modules/ui/plugins.lua
+++ nvim/lua/modules/ui/plugins.lua
@@ -9,6 +9,9 @@ plugin({
   'yorik1984/newpaper.nvim',
   priority = 1000,
   config = function()
+    require('newpaper').setup({
+      style = 'dark',
+    })
     vim.cmd.colorscheme('newpaper')
   end,
 })
